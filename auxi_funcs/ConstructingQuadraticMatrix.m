b018df26501b68513554daf8e2fe9571d8499a5b
yulisun <49111172+yulisun@users.noreply.github.com>
2025-09-24 10:35:28 +0800

    Update ConstructingQuadraticMatrix.m

--- auxi_funcs/ConstructingQuadraticMatrix.m
+++ auxi_funcs/ConstructingQuadraticMatrix.m
@@ -16,23 +16,23 @@ for i = 1: N
     th_x(i) = max(Sdist_t1(i,:));
     th_y(i) = max(Sdist_t2(i,:));
 end
-fuhao_t1 = sign(dist_t2- repmat(th_y,[N 1]));
-fuhao_t2 = sign(dist_t1- repmat(th_x,[N 1]));
+sign_t1 = sign(dist_t2- repmat(th_y,[N 1]));
+sign_t2 = sign(dist_t1- repmat(th_x,[N 1]));
 
 f1 = S_t1.*dist_t2 ;
 f3 = S_t2.*dist_t1 ;
-f1 =  f1.*((fuhao_t1+1)/2);
-f3 =  f3.*((fuhao_t2+1)/2);
+f1 =  f1.*((sign_t1+1)/2);
+f3 =  f3.*((sign_t2+1)/2);
 
 f2 = S_t1.*exp(-dist_t2) ;
 f4 = S_t2.*exp(-dist_t1) ;
-f2 =  f2.*((-fuhao_t1+1)/2);
-f4 =  f4.*((-fuhao_t2+1)/2);
+f2 =  f2.*((-sign_t1+1)/2);
+f4 =  f4.*((-sign_t2+1)/2);
 
 f5 = R_t1.*exp(-dist_t2) ;
 f6 = R_t2.*exp(-dist_t1) ;
-f5 =  f5.*((-fuhao_t1+1)/2);
-f6 =  f6.*((-fuhao_t2+1)/2);
+f5 =  f5.*((-sign_t1+1)/2);
+f6 =  f6.*((-sign_t2+1)/2);
 
 f1 = f1-diag(diag(f1));
 f2 = f2-diag(diag(f2));
@@ -45,5 +45,6 @@ f6 = f6-diag(diag(f6));
 Batt = f1 +   (sum(f1(:))/sum(f3(:))) * f3;
 Brep = f5 +   (sum(f5(:))/sum(f6(:))) * f6;
 Bsmo = f2 +   (sum(f2(:))/sum(f4(:))) * f4;
+
 
 
